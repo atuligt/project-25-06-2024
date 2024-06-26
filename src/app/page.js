5a744ed75fc086dcde373df6956a21ebf4cd06aa
Alokgit09 <130595475+Alokgit09@users.noreply.github.com>
2024-06-26 18:35:34 +0530

    issues solved

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Hero from "./components/Hero";
 import Masonmry from "./components/Masonry";
 import Philosophy from "./components/Philosophy";
 import Service from "./components/Service";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
